83884a26293eba065b171badc174149db4ddf1f6
Yael Kaufman <ylkauf@gmail.com>
2023-04-16 16:45:49 -0700

    text margin

--- src/components/Resources.jsx
+++ src/components/Resources.jsx
@@ -32,7 +32,7 @@ export default function Resources() {
             </div>
             <button className=" m-[auto] block mt-4 hover:scale-105 p-2 rounded-lg bg-pink-200" onClick={navBack}><h5 className=" text-center text-xs font-semibold uppercase">Back</h5></button>
             <div className="flex flex-cols pt-3 bg-white justify-center p-5 text-justify font-light ">
-                <div className="w-[40vw] p-5">
+                <div className="sm:w-[40vw] p-5">
                     <h3>This website was created with the intention of making seasonal eating feel less like work</h3>
                     <img src="https://cdn.dribbble.com/users/2054184/screenshots/4885841/media/8dc03a0c6d0b839db02e59b440eb1e6d.gif" /> <span>and a little more like FUN! </span> <img src="https://media3.giphy.com/media/dAdA5PgN0ovYJLTQ4C/giphy.gif?cid=ecf05e47dw2xykk5vsytd83frzx8fi2v0m30x034slaw637n&rid=giphy.gif&ct=g" />
                     <h5>Not only does eating seasonally mean you'll get the most delicious and nutritious produce around, but you could also be helping your local community of farmers, and doing some good for the environment!</h5><br></br>
