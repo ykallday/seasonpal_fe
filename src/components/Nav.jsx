1e169c479ed9a48054bb1968e115e64ff6333289
Yael Kaufman <ylkauf@gmail.com>
2023-04-16 14:43:04 -0700

    testing mobile styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -20,16 +20,16 @@ export default function Nav() {
         return (
             <div className="h-30 bg-white bg-opacity-10">
                 <div>
-                    <img className=" w-[20vw] h-fit float-left p-9" src={Logo} />
+                    <img className=" w-[20vw] hidden h-fit float-left p-9" src={Logo} />
 
                 </div>
                 <div className="flex justify-end p-6 text-center">
-                    <Link className="m-3 bg-mylime px-2 py-2 pt-2.5 sm:shadow-l rounded-md text-xs hover:scale-105" to="/"><button>HOME</button></Link>
-                    <Link className="m-3 bg-mylime px-2 py-2 pt-2.5 sm:shadow-l rounded-md text-xs hover:scale-105" to="/resources"><button>RESOURCES</button></Link>
-                    <Link className="m-3 bg-mylime px-2 pt-2.5 py-2 sm:shadow-l rounded-md text-xs hover:scale-105" to="/search"><button>SEARCH</button></Link>
-                    <Link className="m-3 bg-mylime px-2 py-1 pt-2 sm:shadow-l rounded-md text-xs hover:scale-105" to="/contact"><button><MdAlternateEmail size={18} /></button></Link>
-                    <Link className="m-3 bg-mylime px-2 py-1 pt-2 sm:shadow-l rounded-md text-xs hover:scale-105" to="/profile"><button><FaUserCircle size={18} /></button></Link>
-                    <Link className="m-3 bg-mylime px-2  py-1 pt-2 sm:shadow-l rounded-md text-xs hover:scale-105" to="/"><button onClick={handleLogout}><HiOutlineLogout size={18} /></button></Link>
+                    <Link className="m-3 bg-mylime px-2 py-2 pt-2.5 rounded-md text-xs hover:scale-105" to="/"><button>HOME</button></Link>
+                    <Link className="m-3 bg-mylime px-2 py-2 pt-2.5  rounded-md text-xs hover:scale-105" to="/resources"><button>RESOURCES</button></Link>
+                    <Link className="m-3 bg-mylime px-2 pt-2.5 py-2 shadow-l rounded-md text-xs hover:scale-105" to="/search"><button>SEARCH</button></Link>
+                    <Link className="m-3 bg-mylime px-2 py-1 pt-2 shadow-l rounded-md text-xs hover:scale-105" to="/contact"><button><MdAlternateEmail size={18} /></button></Link>
+                    <Link className="m-3 bg-mylime px-2 py-1 pt-2 shadow-l rounded-md text-xs hover:scale-105" to="/profile"><button><FaUserCircle size={18} /></button></Link>
+                    <Link className="m-3 bg-mylime px-2  py-1 pt-2 shadow-l rounded-md text-xs hover:scale-105" to="/"><button onClick={handleLogout}><HiOutlineLogout size={18} /></button></Link>
                 </div>
             </div>
         )
@@ -39,7 +39,7 @@ export default function Nav() {
         if (seasonLocations.length == 0) {
             return (
                 <div>
-                    <Link to="/"><img className=" w-[20vw] h-fit p-9" src={Logo} /></Link>
+                    <Link to="/"><img className="sm:w-fit w-[20vw] h-auto p-9" src={Logo} /></Link>
                 </div>
             )
         }
@@ -50,7 +50,7 @@ export default function Nav() {
                         <img className=" w-[20vw] h-fit float-left p-9" src={Logo} />
                     </div>
                     <div className="flex justify-end p-6">
-                        <Link className="m-3 bg-mylime px-2 py-2 pt-2.5 sm:shadow-l rounded-md text-xs hover:scale-105 " to="/"><button>HOME</button></Link>
+                        <Link className="m-3 bg-mylime px-2 py-2 pt-2.5 shadow-l rounded-md text-xs hover:scale-105 " to="/"><button>HOME</button></Link>
                         <Link className="m-3 bg-mylime px-2 py-2 pt-2.5 rounded-md text-xs hover:scale-105" to="/resources"><button>RESOURCES</button></Link>
                         <Link className="m-3 bg-purple-200 px-2 py-2 pt-2.5 rounded-md text-xs tracking-wide hover:scale-105 " to="/login"><button>LOGIN</button></Link>
                         <Link className="m-3 bg-purple-200 px-2 py-2 pt-2.5 rounded-md text-xs hover:scale-105" to="/register"><button>REGISTER</button></Link>
